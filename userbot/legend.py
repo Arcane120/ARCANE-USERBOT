52628293a8a302c28bdc34c8320d06533ca82374
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-04-22 14:07:58 +0530

    Add files via upload

---
+++ userbot/legend.py
@@ -0,0 +1,50 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔🤔🤔 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+#import krte h pehle 👇
+import asyncio
+from telethon import events
+from telethon.tl.types import ChannelParticipantsAdmins
+from userbot import ALIVE_NAME, StartTime
+from userbot.utils import admin_cmd
+from userbot import bot
+from telethon import version
+from math import ceil
+import json
+import random
+import re
+from telethon import events, errors, custom
+import io
+from userbot.helpers.functions import get_readable_time
+import time
+import os
+import datetime
+#importing finished
+from userbot import botnickname 
+BOT = str(botnickname) if botnickname else "𝙳𝙴𝚅𝙸𝙻 𝚄𝚂𝙴𝚁𝙱𝙾𝚃"
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "𝙳𝙴𝚅𝙸𝙻"
+tim = get_readable_time((time.time() - StartTime))
+#pic 👇
+PIC = os.environ.get("ALIVE_PIC")
+#op 
+uptime = tim
+#time = date + time okay
+TIME = time.asctime(time.localtime())
+#my name 👇
+𝗗𝗘𝗩𝗜𝗟 = "[𝗗𝗲𝘃𝗶𝗹](https://t.me/luciifeermorningstar)"
+#my bots repo 👇
+REPO = "[𝗗𝗘𝗩𝗜𝗟 𝗕𝗢𝗧](https://github.com/luciifeermorningstar/Devil)"
+#grpup👇NAME = "[{MAATER}](tg://user?id={X})"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
+global ghanti
+X = bot.uid
+MASTER = f"[{NAME}](tg://user?id={X})"
+GROUP = "[SUPPORT GROUP](https://t.me/deviluserbot)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "∂єνιℓ υѕєяησт ιѕ ση🔥 ƒιяє 🔥" #make by ∂єνιℓ
+OP = "𝙷𝙴𝙻𝙻𝙾 𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 𝙳𝙴𝚅𝙸𝙻 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 𝙸 𝙰𝙼 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚄𝚂𝙴𝚁𝙱𝙾𝚃💝"
+EMOJI = "🔥"
