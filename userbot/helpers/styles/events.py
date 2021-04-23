89f4e9b6d5047548ed540afd13e6e6afb36f79be
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-04-23 21:00:27 +0530

    Update events.py

--- userbot/helpers/styles/events.py
+++ userbot/helpers/styles/events.py
@@ -86,7 +86,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**USERBOT ERROR REPORT**\n"
-                    link = "[Deviluserbot Official Chat](https://t.me/deviluserbot)"
+                    link = "[ARCANE USERBOT Official Chat](https://t.me/Arcane_Bot_Support)"
                     text += "If you want to, you can report it"
                     text += f"- just forward this message to {link}.\n"
                     text += "Nothing is logged except the fact of error and date\n"
