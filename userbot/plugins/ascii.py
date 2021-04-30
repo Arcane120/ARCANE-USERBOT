1ff2533baf319c7c8e0ff85bce24268041aca7f8
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-01 00:41:36 +0530

    Update ascii.py

--- userbot/plugins/ascii.py
+++ userbot/plugins/ascii.py
@@ -9,7 +9,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import CMD_HELP, ALIVE_NAME
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Hell User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Arcane User"
 
 USERID = bot.uid
 
