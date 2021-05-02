c0eca59ade041d5fe400ac53b0c140c33dadbac9
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-02 22:47:00 +0530

    Update arcane.py

--- userbot/plugins/arcane.py
+++ userbot/plugins/arcane.py
@@ -43,15 +43,15 @@ async def amireallyalive(arcane):
    """ For .arcane command, check if the bot is running or not.  """
    tag = borg.uid
    uptm = await legend.get_readable_time((time.time() - StartTime))
-   ALIVE_MESSAGE= f" 🤖 {BOT} 🤖 𝑰𝑺 𝑼𝑷 𝑨𝑵𝑫 𝑹𝑬𝑨𝑫𝒀 𝑻𝑶 𝑺𝑬𝑹𝑽𝑬 𝒀𝑶𝑼⚓.  "
+   ALIVE_MESSAGE= f" ༒︎ {BOT} ༒︎ 𝑰𝑺 𝑼𝑷 𝑨𝑵𝑫 𝑹𝑬𝑨𝑫𝒀 𝑻𝑶 𝑺𝑬𝑹𝑽𝑬 𝒀𝑶𝑼⚓.  "
    ALIVE_MESSAGE += "\n\n"
-   ALIVE_MESSAGE += "🚨 𝚂𝚈𝚂𝚃𝙴𝙼 𝚂𝚃𝙰𝚃𝚄𝚂 🚨\n\n"
-   ALIVE_MESSAGE += "☎️ 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ☎️ : 1.19.5\n\n"
-   ALIVE_MESSAGE += "✨ 𝙰𝚁𝙲𝙰𝙽𝙴 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ✨ :   2.0\n\n"
-   ALIVE_MESSAGE += f"💀 𝚄𝙿𝚃𝙸𝙼𝙴 💀 : {uptm}\n\n"
-   ALIVE_MESSAGE += f"🔥 𝙼𝚈 𝙱𝙾𝚂𝚂 🔥: [{DEFAULTUSER}](tg://user?id={tag})\n\n"
-   ALIVE_MESSAGE += "⚓ 𝙶𝚁𝙾𝚄𝙿 ⚓ : [SUPPORT](https://t.me/Arcane_Bot_Support)\n\n"
-   ALIVE_MESSAGE += f"😍 [𝙳𝙴𝙿𝙻𝙾𝚈](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FArcane120%2Heroku-Setup&template=https%3A%2F%2Fgithub.com%2FArcane120%2FHeroku-Setup) 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙾𝙿 [{BOT}](https://github.com/Arcane120/ARCANE-USERBOT)  💠\n"   
+   ALIVE_MESSAGE += "༆ 𝚂𝚈𝚂𝚃𝙴𝙼 𝚂𝚃𝙰𝚃𝚄𝚂 ༆\n\n"
+   ALIVE_MESSAGE += "𖣔 𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 𖣔 : 1.19.5\n\n"
+   ALIVE_MESSAGE += "★ 𝙰𝚁𝙲𝙰𝙽𝙴 𝚅𝙴𝚁𝚂𝙸𝙾𝙽 ★ :   2.0\n\n"
+   ALIVE_MESSAGE += f"𖣘 𝚄𝙿𝚃𝙸𝙼𝙴 𖣘 : {uptm}\n\n"
+   ALIVE_MESSAGE += f"꧁ 𝙼𝚈 𝙱𝙾𝚂𝚂 ꧂: [{DEFAULTUSER}](tg://user?id={tag})\n\n"
+   ALIVE_MESSAGE += "☯︎ 𝙶𝚁𝙾𝚄𝙿 ☯︎ : [SUPPORT](https://t.me/Arcane_Bot_Support)\n\n"
+   ALIVE_MESSAGE += f"༄ [𝙳𝙴𝙿𝙻𝙾𝚈](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FArcane120%2Heroku-Setup&template=https%3A%2F%2Fgithub.com%2FArcane120%2FHeroku-Setup) ༄ 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 𝙾𝙿 ❄︎[{BOT}](https://github.com/Arcane120/ARCANE-USERBOT)  ❄︎\n"   
    await arcane.delete() 
    await borg.send_file(arcane.chat_id, ALIVE_PHOTTO,caption=ALIVE_MESSAGE)
 
