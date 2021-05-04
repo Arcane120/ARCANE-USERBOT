9a718b764e50a51eded59e62dfe83b3329534fb1
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-05 04:28:34 +0530

    Update botadmin.py

--- userbot/plugins/assistant/botadmin.py
+++ userbot/plugins/assistant/botadmin.py
@@ -1,4 +1,4 @@
-from ULTRAX import xbot, devs as DEVS
+from ARCANE import xbot, devs as DEVS
 
 from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest
