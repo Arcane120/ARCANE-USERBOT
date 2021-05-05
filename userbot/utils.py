4016191b4a8bb07c8de8146c2e7d4e9881b780af
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-06 02:37:55 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -377,7 +377,7 @@ def errors_handler(func):
 
             ftext += "\n\n\nLast 5 commits:\n"
 
-   process = await asyncio.create_subprocess_shell(
+            process = await asyncio.create_subprocess_shell(
                 command,
                 stdout=asyncio.subprocess.PIPE,
                 stderr=asyncio.subprocess.PIPE)
