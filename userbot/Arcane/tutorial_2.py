64168a0714266db6a36f9c12d13b40d36fa98cad
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-05 16:47:12 +0530

    Update tutorial_2.py

--- userbot/Arcane/tutorial_2.py
+++ userbot/Arcane/tutorial_2.py
@@ -1,7 +1,7 @@
 @ultroid_cmd(pattern="tag")
 async def hehe (event):
   Arcane = await event.get_reply_message()
-  X = Legend.sender
+  X = Arcane.sender
   pro = X.first_name
   boy = X.last_name
   await event.edit(f"HELLO BRO THIS IS YOUR ID : {X.id}\n AND THIS IS YOUR USERNAME : {X.username} \n AND THIS IS YOUR NAME : {pro} {boy}")
