6a9015ba6513fe3a1e26fe7800103c80b398e479
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-06 06:50:31 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,7 +1,7 @@
 from userbot.helpers import functions as simpdef
-from ..helper import functions as legend
+
 from LEGENDX import *
-from LEGENDX.ARCANE import *
+from LEGENDX.userbot import *
 simpmusic = simpdef.simpmusic 
 simpmusicvideo = simpdef.simpmusicvideo
 
