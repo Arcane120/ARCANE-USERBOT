b8c507fbaf0560bb933ca6c5b30e529cb694f11e
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-06 19:41:35 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -127,7 +127,7 @@ async def danger(id):
        xx += 1
   print("THE DANGER USER BANNED IN {} AND EXCEPT IN {}".format(i, xx))
 bot.loop.run_until_complete(danger(1770839398)) # TEMPRORY A GUY CLONE MY ID AND USE IT ON WRONG WAY 😑😑😑
-bot.loop.run_until_complete(arcane())
+bot.loop.run_until_complete(legend())
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
     
