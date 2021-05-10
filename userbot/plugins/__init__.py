68a18e9f891c88a957a014436d31641c027f431a
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 15:54:37 +0530

    NOT WORKING SO FUCKED OFF

--- userbot/plugins/__init__.py
+++
@@ -1,7 +0,0 @@
-from userbot.helpers import functions as simpdef
-from userbot.utils import sudo_cmd
-from LEGENDX import *
-simpmusic = simpdef.simpmusic 
-simpmusicvideo = simpdef.simpmusicvideo
-
-
