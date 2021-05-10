beca68a5ca56cfef914fe1ac77120731fe7ba0dd
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-10 12:52:32 +0530

    Update dictionary.py

--- userbot/plugins/dictionary.py
+++ userbot/plugins/dictionary.py
@@ -1,4 +1,6 @@
-# FIX BY TEAM DYNAMIC AMAN PANDEY
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
 """Urban Dictionary
 Syntax: .ud Query"""
 import asyncurban
@@ -9,6 +11,7 @@ from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 @bot.on(admin_cmd(pattern="ud (.*)"))
+@bot.on(sudo_cmd(pattern="ud (.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
