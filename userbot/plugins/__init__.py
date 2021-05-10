53134dad013d6841b3c37a7b128a746976452d9a
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 15:49:24 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,5 +1,5 @@
 from userbot.helpers import functions as simpdef
-
+from userbot.utils import sudo_cmd
 from LEGENDX import *
 simpmusic = simpdef.simpmusic 
 simpmusicvideo = simpdef.simpmusicvideo
