230cd9d411fff6c0517535e28f3adecdfeb26b1b
Aman Pandey Devloper <72003733+amanpandey7647@users.noreply.github.com>
2021-05-10 15:25:27 +0530

    Update Arcane.py

--- Arcane.py
+++ Arcane.py
@@ -1,12 +1,3 @@
-# COPYRIGHT (C) 2021 BY LEGENDX22
-"""
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-(((((((((((((((((((((((@LEGENDX22)))))))))))))))))))))))))))
-"""
-# MADE BY LEGENDX22 🔥
-# MY IDEA H YRR DONT KANG THIS PLEASE
 import asyncio
 import os
 try:
@@ -16,27 +7,27 @@ except:
   from pyrogram import Client, idle
 
 import asyncio
-from userbot.utils import admin_cmd 
-from userbot import bot as ARCANEBOT
+from DYNAMIC import bot as GODBOYX
 API_ID = os.environ.get("APP_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 from telethon import events, custom, Button, TelegramClient
 import time
-from userbot import botnickname, ALIVE_NAME, bot
+from DYNAMIC import ALIVE_NAME, bot
 token = os.environ.get("TG_BOT_TOKEN_BF_HER", None)
-xbot = TelegramClient("legend", API_ID, API_HASH).start(bot_token=token)
-pbot = Client("ARCANE", api_id=API_ID, api_hash=API_HASH, bot_token=token)
-BOT = str(botnickname) if botnickname else "ARCANE BOT"
-NAME = str(ALIVE_NAME) if ALIVE_NAME else "ARCANE BOT"
-PHOTO = os.environ.get("ALIVE_PHOTTO", None)
-ARCANE = "[ARCANE](https://t.me/Arcane_Bot_Support)"
-VERSION = "3.1.5"
-REPO = "[ARCANE BOT](https://github.com/Arcane120/ARCANE-USERBOT)"
-PRO = bot.uid
-MASTER = f"[{NAME}](tg://user?id={PRO})"
-GROUP = "[SUPPORT GROUP](https://t.me/Arcane_Bot_Support)"
+xbot = TelegramClient("GODBOYX", API_ID, API_HASH).start(bot_token=token)
+pbot = Client("GODBOYX", api_id=API_ID, api_hash=API_HASH, bot_token=token)
+NAME = str(ALIVE_NAME) if ALIVE_NAME else "DYNAMIC USER"
+PHOTO = os.environ.get("ALIVE_PHOTO", None)
+GODBOYX = "[GODBOYX](https://t.me/GODBOYX)"
+VERSION = "1.1.0"
+ID = 1742906647
+REPO = "[Dynamic-Userbot](https://github.com/TeamDynamic/Dynamic-Userbot)"
+devs = 1742906647
+MASTER = f"[{NAME}](tg://user?id={ID})"
+GROUP = "[SUPPORT GROUP](https://t.me/DYNAMICUSERBOTSUPPORT)"
 if __name__=="__main__":
   xbot.run_until_disconnected()
 
 if __name__=="__main__":
   pbot.start()
+  run()
