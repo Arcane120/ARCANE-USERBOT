ada25e0cae4e3f138c358fae9be6f54ad13b2fe7
Arcane <82821994+Arcane120@users.noreply.github.com>
2021-05-11 10:52:46 +0530

    Update button.py

--- userbot/plugins/button.py
+++ userbot/plugins/button.py
@@ -9,19 +9,19 @@
                DONT REMOVE THIS LINES
 """
 from userbot.utils import admin_cmd
-from LEGENDX import xbot, NAME
+from Arcane import xbot, NAME
 from telethon import Button
 @borg.on(admin_cmd(pattern="button (.*)"))
 async def Buttons(event):
   await event.edit("making your button")
-  LEGENDX = Var.TG_BOT_USER_NAME_BF_HER
+  Arcane = Var.TG_BOT_USER_NAME_BF_HER
   pro = event.text[7:]
   pro, boy = pro.split("|")
-  if "LEGENDX" == "PROBOYX":
+  if "Arcane" == "PROBOYX":
     await xbot.send_message(event.chat_id, "buttons")
   else:
     try:
-      async with bot.conversation(LEGENDX) as proboyx:
+      async with bot.conversation(Arcane) as proboyx:
         await proboyx.send_message("/start")
         await proboyx.get_response()
         await proboyx.send_message("my button 🥺")
@@ -30,4 +30,4 @@ async def Buttons(event):
         await pro.forward_to(event.chat_id)
         await event.delete()
     except:
-        await event.edit("example:\n.button b<button name>|<link>\n`.button LEGENDX|https://t.me/LEGENDX22`\nmake sure your name and link no have Useless spece ", link_preview=False)
+        await event.edit("example:\n.button b<button name>|<link>\n`.button Arcane|https://t.me/Arcane_Bot_Support`\nmake sure your name and link no have Useless spece ", link_preview=False)
